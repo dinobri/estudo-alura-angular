dd4fc6b35614d2ea36baf8b0e1e5cdb36a8172bf
Diego Brito <diego.nobri@gmail.com>
2021-08-20 20:34:29 -0300

    Aula 02 - Pasta de nova transferência

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NovaTransferenciaComponent } from './nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 
 @NgModule({
   declarations: [AppComponent, NovaTransferenciaComponent],
