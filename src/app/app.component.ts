6ecf7b1cfcedf9f09d4d83e0e8fd4480e76f5a1b
Diego Brito <diego.nobri@gmail.com>
2021-08-20 21:05:16 -0300

    Aula 03

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
+
+  transferencia: any;
+
+  transferir($event): void {
+    console.log($event);
+    this.transferencia = {};
+    this.transferencia.valor = $event.valor;
+    this.transferencia.destino = $event.destino;
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -9,9 +9,17 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
   transferir(): void {
     console.log('Solicitada nova transferência');
-    console.log('Valor: ' + this.valor);
-    console.log('Destino: ' + this.destino);
+
+    this.aoTransferir.emit({ valor: this.valor, destino: this.destino });
+    this.limparCampos();
+  }
+
+  limparCampos(): void {
+    this.valor = 0;
+    this.destino = 0;
   }
 }
